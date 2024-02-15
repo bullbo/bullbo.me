869d1c4a6c301faad69347cb143fb11b9d160336
Amin Bolakhrif <amin.bullarif@gmail.com>
2024-02-15 22:07:07 +0100

    add subdomain site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://bullbo.github.io",
-  base: "/bullbo.me",
+  site: "https://amin.bullbo.me",
+  // base: "/bullbo.me",
 });
